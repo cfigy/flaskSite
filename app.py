04209ef66d94d06308f6e24310c6bd4b9d17d832
cfigy <135090228+cfigy@users.noreply.github.com>
2023-07-15 16:30:05 +0000

    added some inline styles and start of page ... todo add bootstrap next

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def root():
   return render_template('home.html')
+
+@app.route("/projects")
+def webhook():
+  return render_template('hook.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
